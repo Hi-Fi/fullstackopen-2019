81fe08f655a835ad6173eb1620d2d8dbd9bc57c8
Juho Saarinen <juho.saarinen@gmail.com>
2019-03-24 16:23:06 +0200

    Reactin alkeet -tehtävät

---
+++ osa1/1.2_kurssitiedot_step2/src/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => (
+    <h1>{props.course}</h1>
+)
+
+const Content = (props) => (
+    <>
+        <Part part={props.part1} exercise={props.exercise1} />
+        <Part part={props.part2} exercise={props.exercise2} />
+        <Part part={props.part3} exercice={props.exercise3} />
+    </>
+)
+
+const Part = (props) => (
+    <p>
+            {props.part} {props.exercise}
+    </p>
+)
+
+const Total = (props) => (
+    <p>yhteensä {props.exercises1 + props.exercises2 + props.exercises3} tehtävää</p>
+)
+
+const App = () => {
+  const course = 'Half Stack -sovelluskehitys'
+  const part1 = 'Reactin perusteet'
+  const exercises1 = 10
+  const part2 = 'Tiedonvälitys propseilla'
+  const exercises2 = 7
+  const part3 = 'Komponenttien tila'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} exercise1={exercises1}
+               part2={part2} exercise2={exercises2}
+               part3={part3} exercise3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
