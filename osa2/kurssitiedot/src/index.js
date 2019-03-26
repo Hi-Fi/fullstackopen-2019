da4b0caa140c95c0a9c808d86ffede7ce98bfc28
Juho Saarinen <juho.saarinen@gmail.com>
2019-03-26 18:02:45 +0200

    2.4: kurssitiedot step9

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -37,35 +37,52 @@ const Course = ({course}) => {
 }
 
 const App = () => {
-    const course = {
-      name: 'Half Stack -sovelluskehitys',
-      parts: [
+    const courses = [
         {
-          name: 'Reactin perusteet',
-          exercises: 10,
-          id: 1
+          name: 'Half Stack -sovelluskehitys',
+          id: 1,
+          parts: [
+            {
+              name: 'Reactin perusteet',
+              exercises: 10,
+              id: 1
+            },
+            {
+              name: 'Tiedonvälitys propseilla',
+              exercises: 7,
+              id: 2
+            },
+            {
+              name: 'Komponenttien tila',
+              exercises: 14,
+              id: 3
+            }
+          ]
         },
         {
-          name: 'Tiedonvälitys propseilla',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'Komponenttien tila',
-          exercises: 14,
-          id: 3
-        },
-        {
-            name: 'Redux',
-            exercises: 7,
-            id: 4
-          }
+          name: 'Node.js',
+          id: 2,
+          parts: [
+            {
+              name: 'Routing',
+              exercises: 3,
+              id: 1
+            },
+            {
+              name: 'Middlewaret',
+              exercises: 7,
+              id: 2
+            }
+          ]
+        }
       ]
-    }
   
     return (
       <div>
-        <Course course={course} />
+          <h1>Opetusohjelma</h1>
+          {courses.map ( course => 
+            <Course course={course} key={course.id}/>
+          )}
       </div>
     )
   }
