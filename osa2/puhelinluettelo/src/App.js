34265ec4c447c5a9461913a8a2f47fa26df5a1c2
Hi-Fi <juho.saarinen@gmail.com>
2019-05-19 12:54:16 +0300

    Henkilöiden lisäys

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -6,19 +6,39 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const Contacts = (props) => {
+    let i = 0
+    return props.persons.map ( person => 
+        <Contact name={person.name} key={i++}/>
+    )
+    }
+
+const Contact = (props) => (
+    <p>
+        {props.name}
+    </p>
+)
+
+const addPerson = (event) => {
+  event.preventDefault()
+  setPersons(persons.concat({name: newName}))
+  setNewName('')
+}
+
+
   return (
     <div>
       <h2>Puhelinluettelo</h2>
       <form>
         <div>
-          nimi: <input />
+          nimi: <input value={newName} onChange={(event) => setNewName(event.target.value)}/>
         </div>
         <div>
-          <button type="submit">lisää</button>
+          <button type="submit" onClick={addPerson}>lisää</button>
         </div>
       </form>
       <h2>Numerot</h2>
-      ...
+      <Contacts persons={persons} />
     </div>
   )
 
