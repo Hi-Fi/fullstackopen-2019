5ce60e52b4bd7c9b0a83244f5d93e2c64462c614
Juho Saarinen <juho.saarinen@gmail.com>
2019-06-02 11:29:58 +0300

    4.9*: blogilistan testit, step2

--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -1,14 +1,35 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
 const api = supertest(app)
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('when there is initially some notes saved', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    const blogObjects = helper.initialBlogs
+      .map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+  })
+
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('blog identified field is id', async () => {
+    let response = await api.get('/api/blogs')
+    
+    response.body.map ( blog => {
+      expect(blog.id).toBeDefined()
+    })
+      
+  })
 })
 
 afterAll(() => {
