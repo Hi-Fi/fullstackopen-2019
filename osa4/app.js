8c6d1bc49af1e1593cc60218a42cf96cfcbabf56
Juho Saarinen <juho.saarinen@gmail.com>
2019-06-07 12:07:52 +0300

    4.18: blogilistan laajennus, step7

--- osa4/app.js
+++ osa4/app.js
@@ -4,10 +4,12 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const blogRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
